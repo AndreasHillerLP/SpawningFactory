f7b8404204b5828a31f419ce16611e2c15804ff8
AndreasHillerLP <kontakt@andreashillerlp.de>
2015-02-12 14:01:30 +0100

    SpawningFactory 1.0.2

--- java/net/andreashiller/minecraft/spawningfactory/SpawningFactory.java
+++ java/net/andreashiller/minecraft/spawningfactory/SpawningFactory.java
@@ -36,7 +36,7 @@ public class SpawningFactory {
 	// MODDATA
 	public static final String MODID = "spawningfactory";
 	public static final String NAME = "SpawningFactory Mod";
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.0.2";
 	
 	// MODINSTANCE
 	@Instance(value=SpawningFactory.MODID)
